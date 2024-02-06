4b3c8366fa5b4776617864d1b5f43df0b0ce08b3
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2024-02-06 17:14:49 -0300

    mudança na rota para https

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://projpsi.pythonanywhere.com/api/psicoapp/',
+  baseURL: 'https://projpsi.pythonanywhere.com/api/psicoapp/',
 });
