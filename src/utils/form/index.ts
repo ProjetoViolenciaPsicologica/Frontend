8e8207debc0892b78e403c8f21a6a7c50e33b2d8
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2024-03-04 21:47:35 +0000

    mudancas no formulario

--- src/utils/form/index.ts
+++ src/utils/form/index.ts
@@ -1,25 +1,30 @@
-export const categories = ["MEDO", "DEPENDÊNCIA EMOCIONAL", "CONTROLE ABSOLUTO (SUBJUGAÇÃO)"]
+export const categories = [
+  "",
+  "MEDO",
+  "DEPENDÊNCIA EMOCIONAL",
+  "CONTROLE ABSOLUTO (SUBJUGAÇÃO)",
+];
 
 export const questions = {
-    MEDO: [
-        '1- Algum familiar ou relacionamento seu causa em você um constante estado de ansiedade, de se sentir "pisando em ovos", com pânico, mal-estar, palpitações? ',
-        `2- Algum familiar ou relacionamento seu provoca caos na sua vida, conflitos intermináveis, fazendo "tempestade em copo d'água" por qualquer coisa? `,
-        `3- Algum familiar ou relacionamento seu ameaça você com o medo da perda das pessoas e coisas que você mais estima?`,
-    ],
-    DEPENDENCIA: [
-        `4- Algum familiar ou relacionamento seu  humilha você e insiste que você não tem valor, diretamente ou comparando você com outras pessoas?`,
-        `5- Algum familiar ou relacionamento seu insiste que você faça e se esforce mais, mais e mais por ele e absolutamente nada nunca é bom o suficiente, ao passo que ele faz e se esforça menos, menos e menos por você? `,
-        `6- Algum familiar ou relacionamento seu castiga você com explosões de fúria? `,
-        `7- Algum familiar ou relacionamento seu castiga você com períodos longos de silêncio, dando-lhe "gelo" e passando a agir como se você não existisse? `,
-        `8- Algum familiar ou relacionamento seu faz você se sentir como se estivesse ficando maluco, provocando sumiço de coisas, mentindo e se contradizendo repetidamente?`,
-        `9 - Algum familiar ou relacionamento seu acusa você de coisas erradas que é ele próprio que faz? `,
-        `10- Algum familiar ou relacionamento seu faz questão de estragar todas as suas conquistas e datas comemorativas? `
-    ],
-    CONTROLE: [
-        `11- Algum familiar ou relacionamento seu exige de você submissão total, tendo que aceitar sem reclamar toda e qualquer exigência dele? `,
-        `12- Algum familiar ou relacionamento seu se diz no direito de governar como você pensa, se veste e se comporta e o que você faz, a que horas deve fazer e com quem pode ter contato, e monitora você todas as horas todos os dias?`,
-        `13- Algum familiar ou relacionamento seu se diz no direito de controlar ou invadir as suas senhas, as suas redes sociais, o seu e-mail, as suas postagens?`,
-        `14- Algum familiar ou relacionamento seu controla ou se aproveita do seu dinheiro, exigindo ser ele a prioridade e que você fique por último na fila das prioridades?`,
-        `15- Algum familiar ou relacionamento seu se esforça por controlar como as pessoas enxergam você, passando a imagem para as pessoas de que você é que é uma pessoa desequilibrada e problemática, um peso, um fardo?`
-    ]
-}
+  MEDO: [
+    '1- Algum familiar ou relacionamento seu causa em você um constante estado de ansiedade, de se sentir "pisando em ovos", com pânico, mal-estar, palpitações? ',
+    `2- Algum familiar ou relacionamento seu provoca caos na sua vida, conflitos intermináveis, fazendo "tempestade em copo d'água" por qualquer coisa? `,
+    `3- Algum familiar ou relacionamento seu ameaça você com o medo da perda das pessoas e coisas que você mais estima?`,
+  ],
+  DEPENDENCIA: [
+    `4- Algum familiar ou relacionamento seu  humilha você e insiste que você não tem valor, diretamente ou comparando você com outras pessoas?`,
+    `5- Algum familiar ou relacionamento seu insiste que você faça e se esforce mais, mais e mais por ele e absolutamente nada nunca é bom o suficiente, ao passo que ele faz e se esforça menos, menos e menos por você? `,
+    `6- Algum familiar ou relacionamento seu castiga você com explosões de fúria? `,
+    `7- Algum familiar ou relacionamento seu castiga você com períodos longos de silêncio, dando-lhe "gelo" e passando a agir como se você não existisse? `,
+    `8- Algum familiar ou relacionamento seu faz você se sentir como se estivesse ficando maluco, provocando sumiço de coisas, mentindo e se contradizendo repetidamente?`,
+    `9 - Algum familiar ou relacionamento seu acusa você de coisas erradas que é ele próprio que faz? `,
+    `10- Algum familiar ou relacionamento seu faz questão de estragar todas as suas conquistas e datas comemorativas? `,
+  ],
+  CONTROLE: [
+    `11- Algum familiar ou relacionamento seu exige de você submissão total, tendo que aceitar sem reclamar toda e qualquer exigência dele? `,
+    `12- Algum familiar ou relacionamento seu se diz no direito de governar como você pensa, se veste e se comporta e o que você faz, a que horas deve fazer e com quem pode ter contato, e monitora você todas as horas todos os dias?`,
+    `13- Algum familiar ou relacionamento seu se diz no direito de controlar ou invadir as suas senhas, as suas redes sociais, o seu e-mail, as suas postagens?`,
+    `14- Algum familiar ou relacionamento seu controla ou se aproveita do seu dinheiro, exigindo ser ele a prioridade e que você fique por último na fila das prioridades?`,
+    `15- Algum familiar ou relacionamento seu se esforça por controlar como as pessoas enxergam você, passando a imagem para as pessoas de que você é que é uma pessoa desequilibrada e problemática, um peso, um fardo?`,
+  ],
+};
