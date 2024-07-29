ef6a3e6edac1072d8dce8b8ead875a5a5b079523
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2024-07-29 14:09:20 -0300

    --fix: Update title and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
-      <body className="h-screen bg-[#F6FBF9]">
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+        <title>Questionário Kurt Mendonça</title>
+        <meta name="description" content="Questionário Kurt Mendonça" />
+      </Head>
+      <body className="h-screen">
         <Main />
         <NextScript />
       </body>
